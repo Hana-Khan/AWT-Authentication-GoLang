12601dff69a97cd8ff214c3f5a54c2f96f13dab3
Hina Khan <47082795+Hana-Khan@users.noreply.github.com>
2023-03-25 22:48:11 +0500

    login controller  added

--- controllers/usercontroller.go
+++ controllers/usercontroller.go
@@ -1,5 +1,6 @@
 package controllers
 import (
+	"jwt-authentication-golang/auth"
 	"jwt-authentication-golang/database"
 	"jwt-authentication-golang/models"
 	"net/http"
@@ -30,4 +31,37 @@ func RegisterUser(context *gin.Context) {
 	}
 	// Finally, if everything goes well, we send back the user id, name, and email to the client along with a 200 SUCCESS status code.
 	context.JSON(http.StatusCreated, gin.H{"userId": user.ID, "email": user.Email, "username": user.Username})
+}
+func Login(context *gin.Context) {
+	var request TokenRequest
+	var user models.User
+	if err := context.ShouldBindJSON(&request); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+	// check if email exists and password is correct
+	record := database.Instance.Where("email = ?", request.Email).First(&user)
+	if record.Error != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": record.Error.Error()})
+		context.Abort()
+		return
+	}
+	credentialError := user.CheckPassword(request.Password)
+	if credentialError != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"error": "invalid credentials"})
+		context.Abort()
+		return
+	}
+
+	// If everything goes well, and the password is matched, 
+	//  we Generate the JWT using the GenerateJWT() function. 
+	// This would return a signed token string with an expiry of 1 hour, which in turn will be sent back to the client as a response with a 200 Status Code.
+	tokenString, err:= auth.GenerateJWT(user.Email, user.Username)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"user":gin.H{"userId": user.ID, "email": user.Email, "username": user.Username},"token": tokenString})
 }
